8e2a0d868c1b9dfc4f4e5cbff12dbd5d078b77c1
Anastasia Tsegelskaya <muaaaaaaa@mail.ru>
2020-01-30 08:36:45 +0300

    remove antd styles import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ export { FormBuilder } from './components/FormBuilder';
 export { withElementWrapper } from './hocs/withElementWrapper';
 
 import 'font-awesome/css/font-awesome.min.css';
-import 'antd/dist/antd.css';
 import 'draft-js/dist/Draft.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
